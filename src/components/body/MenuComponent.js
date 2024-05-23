d457dd4c3000d1ee5ece64069b47d0053b04cb34
Aftab <aftabrafi9@gmail.com>
2024-05-23 13:58:29 +0600

    body part: dish details and dateformat

--- src/components/body/MenuComponent.js
+++ src/components/body/MenuComponent.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import MenuItem from "./MenuItem";
 import DISHES from '../../data/dishes';
+import DishDetail from './DishDetail';
 
 
 const MenuComponent = () => {
     const [dishes] = useState(DISHES);
+    const [selectedDish, setSelectedDish] = useState(null);
+    const onSelectDish = (dish) => {
+        setSelectedDish(dish);
+    };
     const menu = dishes.map((dish) => {
-        return <MenuItem dish={dish} key={dish.id} />
+        return <MenuItem dish={dish} onSelectDish={onSelectDish} key={dish.id} />
     });
+    const dishDetail = selectedDish ? <DishDetail dish={selectedDish} /> : null;
     return (
         <div className="container ">
             <div className="row">
-                <div className="col-5">
-                    {menu}
-                </div>
+                <div className="col-5">{menu}</div>
+                <div className="col-7">{dishDetail} </div>
             </div>
         </div>
     );
--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const MenuItem = ({ dish }) => {
+const MenuItem = ({ dish, onSelectDish }) => {
     return (
         <div>
-            <Card inverse style={{ padding: 10, margin: 10 }}>
+            <Card inverse style={{ padding: 10, margin: 10, cursor: 'pointer' }} onClick={() => onSelectDish(dish)}>
                 <CardImg
                     alt={dish.name}
                     src={dish.image}
